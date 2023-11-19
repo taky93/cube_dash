218af70cf6212bde65135578dd5773d2920d3761
taky93 <takybre93@gmail.com>
2023-11-19 20:48:02 +0100

    Simple player object implemented

---
+++ Cube Dash/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Vector3 move;
+    public float speed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = new Vector3(0, 0, 10);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(move * speed * Time.deltaTime,ForceMode.Acceleration);
+    }
+}
